c0d74acb7556c2da1281bf0cd23cb8a7a6034951
Wojciech Kazaniecki <kontakt.wolfeq@gmail.com>
2026-05-05 21:19:12 +0200

    fixes in shared lib

--- shared/player.h
+++ shared/player.h
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-#pragma once
-
-class player
-{
-private:
-    //Player Stats
-    float x;
-    float y;
-    int speed;
-    float stamina;
-    bool isCaught;
-    bool isSeeker;
-    bool isConnected;
-    float abilityCooldown;
-
-    //Client Input
-    bool up;
-    bool down;
-    bool right;
-    bool left;
-    bool shift;
-    //bool e_key;
-    
-public:
-    //Constructor, Deconstructor
-    player();
-    ~player();
-
-    //Getters
-    float getX() const;
-    float getY() const;
-    float getStamina() const;
-    float getAbilityCooldown() const;
-    int getSpeed() const;
-    bool getIsCaught() const;
-    bool getIsSeeker() const;
-    bool getIsConnected() const;
-
-    //Setters
-    void setX(float);
-    void setY(float);
-    void setStamina(float);
-    void setAbilityCooldown(float);
-    void setSpeed(int);
-    void setIsCaught(bool);
-    void setIsSeeker(bool);
-    void setIsConnected(bool);
-
-    //methods
-    bool checkCollision(player, player);
-};
-=======
 #pragma once
 
 struct ClientInput {
@@ -117,4 +64,3 @@ private:
     ClientInput input;
 
 };
->>>>>>> b75cba87813cb24104d286244f27e2026e1c7c22
