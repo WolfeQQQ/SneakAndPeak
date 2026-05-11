d1b2384a836514c1824b0a4a0cec8818fb541865
Wojciech Kazaniecki <kontakt.wolfeq@gmail.com>
2026-05-12 01:43:56 +0200

    added working stamina system in client

--- client/inGameScreen.cpp
+++ client/inGameScreen.cpp
@@ -61,7 +61,7 @@ void InGameScreen::draw(){
         
         
     DrawText("Stamina: ", 10, 10, 20,WHITE);
-    // DrawRectangle(10,40, myPlayer.getStamina() * 2, 20, GREEN);
+    DrawRectangle(10,40, myPlayer.getStamina() * 2, 20, GREEN);
     DrawRectangleLines(10,40, 200, 20, WHITE);
 
     
