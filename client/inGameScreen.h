816c8b062f14f3f5df7816963dc7ea974c226ce0
Wojciech Kazaniecki <kontakt.wolfeq@gmail.com>
2026-05-19 22:58:49 +0200

    Added player sprite and Animations!!!

--- client/inGameScreen.h
+++ client/inGameScreen.h
@@ -5,8 +5,44 @@
 #include "raylib.h"
 #include "tilemap.h"
 
+
+enum class PlayerState {
+    IDLE,
+    WALK,
+    DEATH
+};      //TEMP TO BE IMPLEMENTED IN SERVER
+enum class PlayerDirection {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};      //TEMP TO BE IMPLEMENTED IN SERVER
+
+struct PlayerAnimation {
+    PlayerState lastState = PlayerState::IDLE;
+    PlayerState state = PlayerState::IDLE;
+    PlayerDirection direction = PlayerDirection::DOWN;
+
+    int frame = 0;
+    float frameTimer = 0.0f;
+
+};
+
+
+
+
 class InGameScreen : public Screen {
     private:
+        Texture2D playerIdleTexture;
+        Texture2D playerWalkTexture;
+        Texture2D playerDeathTexture;
+
+        Texture2D seekerIdleTexture;
+        Texture2D seekerWalkTexture;
+        Texture2D seekerDeathTexture;
+
+        PlayerAnimation playerAnim[4];
+
         Camera2D camera;
         player myPlayer;
         player players[4]; 
@@ -17,5 +53,5 @@ class InGameScreen : public Screen {
         InGameScreen(Network* networkClient, int playerId);
         AppState update() override;
         void draw() override;
-    
+        ~InGameScreen();
 };
