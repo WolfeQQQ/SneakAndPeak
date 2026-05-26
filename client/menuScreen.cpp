62e06f107d33dbee19a52a512bcbc0d3a12c209f
Wojciech Kazaniecki <kontakt.wolfeq@gmail.com>
2026-05-27 01:17:18 +0200

    Modified the Main menu - Added the walking player animation and second map for the menu

--- client/menuScreen.cpp
+++ client/menuScreen.cpp
@@ -1,7 +1,80 @@
 #include "menuScreen.h"
+#include <cmath>
+#include <string>
+#include <vector>
+#include "../shared/contstants.h"
+#include "raylib.h"
+
+
+#define MENU_CSV_PATH "assets/Menu.csv"
+
+//Constructor initializes textures, shaders etc. for the menu screen
+MenuScreen::MenuScreen() {
+    // Load textures for the animated menu
+    hiderWalkTexture = LoadTexture(PLAYER_WALK_PATH);
+
+    // Load the tilemap
+    menuMap.load(MENU_CSV_PATH, TILESET_PATH, TILE_SIZE);
+
+    // Prepare the canvas for dynamic lighting effect
+    lightMask = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    // Prepare the shader for the dynamic lighting effect 
+    visionShader = LoadShader(0, VISION_SHADER_PATH);
+    playerPosLoc = GetShaderLocation(visionShader, "playerPos");
+    ResolutionLoc = GetShaderLocation(visionShader, "resolution");
+    radiusLoc = GetShaderLocation(visionShader, "radius");
+    softnessLoc = GetShaderLocation(visionShader, "softness");
+    float resolution[2] = {(float)SCREEN_WIDTH, (float)SCREEN_HEIGHT};
+    SetShaderValue(visionShader, ResolutionLoc, resolution, SHADER_UNIFORM_VEC2); // Set the resolution uniform in the shader because it doesn't change
+
+    camera = { 0 };
+    camera.target = (Vector2){10.0f * TILE_SIZE, 10.0f *TILE_SIZE}; // Lock the camera for better appearance
+    camera.offset = (Vector2){SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = CAMERA_ZOOM;
+
+    std::vector<Vector2> gridWaypoints =  {
+        {20.0f, 5.0f}, 
+        {17.0f, 5.0f}, 
+        {17.0f, 9.0f}, 
+        {16.0f, 9.0f},
+        {16.0f, 5.0f},
+        {11.0f, 5.0f}, 
+        {11.0f, 14.0f}, 
+        {15.0f, 14.0f}, 
+        {15.0f, 12.0f},
+        {20.0f, 12.0f}   
+    };
+
+    // Translate the grid waypoints to World coordinates
+    for(const auto& point: gridWaypoints){
+        waypoints.push_back({ (point.x * TILE_SIZE) + (TILE_SIZE/4), (point.y * TILE_SIZE)  });
+    }
+    
+    // Set the player coordinates to the first waypoint
+    menuPlayer.setX(waypoints[0].x);
+    menuPlayer.setY(waypoints[0].y);
+
+    menuPlayer.setDirection(player::Direction::LEFT);
+    menuPlayer.setPlayerState(player::PlayerState::WALK);
+
+}
+
+// Destructor, unloads everything that was loaded in the constructor
+MenuScreen::~MenuScreen() {
+    UnloadTexture(hiderWalkTexture);
+    menuMap.unload();
+    UnloadRenderTexture(lightMask);
+    UnloadShader(visionShader);
+}
+
 
 // Whole menu update logic. Handles navigation and selection of options. Returns the next state to transition to.
+// UPDATE: Now also handles the animations
 AppState MenuScreen::update(){
+
+    // Input Handling
     if (IsKeyPressed(KEY_DOWN)){
         selectedOption = (selectedOption + 1) % optionsCount;
     }
@@ -20,20 +93,150 @@ AppState MenuScreen::update(){
         }
     }
 
+    // Animations update
+    float deltaTime = GetFrameTime();
+    playerAnim.frameTimer += deltaTime;
+    sceneTimer += deltaTime;
+
+    if (playerAnim.frameTimer >= ANIMATION_SPEED){
+        playerAnim.frameTimer = 0.0f;
+        playerAnim.frame = (playerAnim.frame + 1) % WALK_FRAMES;
+    }
+
+    Vector2 target = waypoints[currentWaypoint];
+    float playerX = menuPlayer.getX();
+    float playerY = menuPlayer.getY();
+
+    float dx = target.x - playerX;
+    float dy = target.y - playerY;
+
+    float distance = sqrtf(dx*dx + dy*dy);
+
+    if (distance < 2.0f){
+        currentWaypoint = (currentWaypoint + 1) % waypoints.size();
+    }
+    else{
+        float speed = 144.0f;
+        float dirX = dx / distance;
+        float dirY = dy / distance;
+
+        menuPlayer.setX(playerX + dirX * speed * deltaTime);
+        menuPlayer.setY(playerY + dirY * speed * deltaTime);
+
+        if(fabs(dirX) > fabs(dirY)){
+            if (dirX > 0){
+                menuPlayer.setDirection(player::Direction::RIGHT);
+            }
+            else menuPlayer.setDirection(player::Direction::LEFT);
+        }
+        else{
+            if(dirY > 0){
+                menuPlayer.setDirection(player::Direction::DOWN);
+            }
+            else menuPlayer.setDirection(player::Direction::UP);
+        }
+
+    }
     return AppState::MAIN_MENU; // Stay on the main menu if no option is selected
 }
 
 // Renders the Main menu
 void MenuScreen::draw(){
 
+    BeginTextureMode(lightMask);
+
+        ClearBackground((Color){ 30, 30, 40, 255 }); 
+
+        BeginMode2D(camera);
+
+             Vector2 center = {menuPlayer.getX() + PLAYER_WIDTH/2, menuPlayer.getY() + PLAYER_LENGTH/2};
+          
+            std::vector<Vector2> points;    // Actual raycasted points
+
+            // Cast Rays in a full circle around the player
+            for(int i=0 ; i<360; i++){ 
+                float rad = i * DEG2RAD;
+                float x = cos(rad);
+                float y = sin(rad);
+
+                Vector2 hit = center; // Initialize the hit point to the center of the player
+
+                // Cast a ray in the current direction until it hits a wall or go beyond the vision radius
+                for (float j = 0; j < VISION_RADIUS; j += 1.0f) {
+                    hit.x = center.x + x * j;
+                    hit.y = center.y + y * j;
+
+                    if(menuMap.isSolid(hit.x, hit.y)){
+                        hit.x += x ; // Move the hit point slightly into the wall
+                        hit.y += y ;
+                        break;
+                    }
+                }
+                points.push_back(hit); // Store the raycasted point
+            }
+
+            // Draw the vision cone using the raycasted points
+            for(size_t i =0; i<points.size()-1; i++){
+                DrawTriangle(center, points[i+1], points[i], WHITE);
+            }
+            DrawTriangle(center, points[0], points.back(), WHITE);
+
+        EndMode2D();
+    EndTextureMode();
+
+    BeginMode2D(camera);
+
+        menuMap.draw();
+        player::Direction direction = menuPlayer.getDirection();
+        int row;
+        if(direction == player::Direction::DOWN) row = 0;
+            else if(direction == player::Direction::LEFT || direction == player::Direction::RIGHT) row = 1;
+            else if(direction == player::Direction::UP) row = 2;
+
+        float frameWidth = PLAYER_FRAME_SIZE;
+        if(direction == player::Direction::LEFT){
+            frameWidth = -PLAYER_FRAME_SIZE; // Flip the texture for left direction
+        }
+
+        Rectangle source = {(float)(playerAnim.frame * PLAYER_FRAME_SIZE), (float)(row * PLAYER_FRAME_SIZE), frameWidth, PLAYER_FRAME_SIZE};
+
+        float scale = 2.0f;
+        float drawSize = PLAYER_FRAME_SIZE * scale;
+        float dX = menuPlayer.getX() - (drawSize / 2) + PLAYER_WIDTH/2;
+        float dY = (menuPlayer.getY() + 1) - (drawSize / 2) + PLAYER_LENGTH/2;
+        Rectangle dest = {dX, dY, drawSize, drawSize};
+        DrawTexturePro(hiderWalkTexture, source, dest, (Vector2){0,0}, 0.0f, WHITE);
+
+    EndMode2D();
+
+    Vector2 wPlayerPos = {menuPlayer.getX()+PLAYER_WIDTH/2, menuPlayer.getY()+PLAYER_LENGTH/2}; // World position of the player, used for shader calculations
+    Vector2 screenPlayerPos = GetWorldToScreen2D(wPlayerPos, camera); // Convert the world position to screen coordinates
+
+    float playerPos[2] = {screenPlayerPos.x, (float)SCREEN_HEIGHT- screenPlayerPos.y};
+    float radiusUniform = VISION_RADIUS;
+    float softness = VISION_SOFTNESS;
+    
+    // Set the shader values for the player's position, vision radius and softness
+    SetShaderValue(visionShader, playerPosLoc, playerPos, SHADER_UNIFORM_VEC2);
+    SetShaderValue(visionShader, radiusLoc, &radiusUniform, SHADER_UNIFORM_FLOAT);
+    SetShaderValue(visionShader, softnessLoc, &softness, SHADER_UNIFORM_FLOAT);
+
+    // Draw the light mask with the vision shader applied to create the shadows
+    BeginBlendMode(BLEND_MULTIPLIED);
+        BeginShaderMode(visionShader); 
+            Rectangle sourceRec = { 0.0f, 0.0f, (float)lightMask.texture.width, -(float)lightMask.texture.height };
+            DrawTextureRec(lightMask.texture, sourceRec, (Vector2){ 0, 0 }, WHITE);
+        EndShaderMode();
+    EndBlendMode();
+
+    //UI ELEMENTS
+    //UI ELEMENTS 
+
     // Colors for the currently selected and non-selected options
     Color startColor = (selectedOption == 0) ? LIGHTGRAY : DARKGRAY; 
     Color exitColor = (selectedOption == 1) ? LIGHTGRAY : DARKGRAY; 
-
-    int screenWidth = GetScreenWidth();
-    int screenHeight = GetScreenHeight();
     
-    DrawText("SNEAK&PEAK", 100, screenHeight / 2 - 200, 100, WHITE); // TODO - center text properly
+    DrawText("SNEAK&PEAK", 100, SCREEN_HEIGHT / 2 - 200, 100, WHITE); // TODO - center text properly
     
     DrawRectangle(100, 300, 200, 50, startColor); 
     DrawText("Start Game", 100, 310, 20, BLACK);   // TODO -  Buttons too...
--- client/menuScreen.h
+++ client/menuScreen.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "screen.h"
 #include "raylib.h"
+#include "../shared/player.h"
+#include "tilemap.h"
+/**
+ * @struct MenuAnimation
+ * @brief Holds the animation state for the main menu
+ */
+struct MenuAnimation{
+    int frame = 0;
+    float frameTimer = 0.0f;
+};
 
 /**
  * @class MenuScreen
@@ -11,7 +21,31 @@ class MenuScreen : public Screen {
         int optionsCount = 2; // Number of menu options
         int selectedOption = 0; // Variable to track the currently selected menu option
 
+        Texture2D hiderWalkTexture; // Texture for the hider's walking animation in the menu
+        Camera2D camera; // Camera for the menu screen, used to create a dynamic background effect
+        Tilemap menuMap; // Mini map for the menu animation
+
+        player menuPlayer;
+        MenuAnimation playerAnim;
+        float sceneTimer;
+
+        //animation
+        std::vector<Vector2> waypoints;
+        size_t currentWaypoint = 0;
+
+        RenderTexture2D lightMask; // Render texture for the light mask, used to create a dynamic lighting effect in the menu background
+        Shader visionShader; // Shader for the lighting effect in the menu background
+
+        int playerPosLoc; // Uniform location for the player's position in the vision shader
+        int ResolutionLoc; // Uniform location for the screen resolution in the vision shader
+        int radiusLoc; // Uniform location for the vision radius in the vision shader
+        int softnessLoc; // Uniform location for the vision softness in the vision shader
+
     public:
+
+        MenuScreen();
+
+        ~MenuScreen();
 
         /**
          * @brief Updates the menu screen based on user input, allowing navigation through options and selection.
